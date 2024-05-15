9932293b0cc5b7434be5015fd608fc45b9e58580
JorgeAntonioJr <jorgeantonio101001@gmail.com>
2024-05-15 19:14:55 -0300

    Atvdd 06

---
+++ exercicio06/src/test/java/com/trabalhosatc/apipagamento/ApiPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalhosatc.apipagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
